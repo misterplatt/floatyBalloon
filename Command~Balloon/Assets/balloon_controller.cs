b3912743f23b4eb993e3e6a72081e69c6f0776fe
Sam Fields <sam.fields94@gmail.com>
2014-10-04 08:54:31 -0700

    Dumping Unity Project

---
+++ Command~Balloon/Assets/balloon_controller.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balloon_controller : MonoBehaviour {
+
+	public float floatSpeed;
+	public float dropPerClick;
+
+	public float addLRSpeed;
+	public float maxLRSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			rigidbody2D.AddForce(new Vector2 (0, -30));		
+		}
+	}
+
+	void FixedUpdate () {
+		//L/R code
+		float moveH = Input.GetAxis ("Horizontal");
+		if(moveH > 0)
+		{
+			if(rigidbody2D.velocity.x <= maxLRSpeed)
+				rigidbody2D.AddForce(new Vector2 (moveH * addLRSpeed, 0));
+		} 
+		else if(moveH < 0)
+		{
+			if(rigidbody2D.velocity.x > -maxLRSpeed)
+				rigidbody2D.AddForce(new Vector2 (moveH * addLRSpeed, 0));
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "wall") {
+			Destroy (gameObject);
+		}
+	}
+
+}
