2af20fca7bd9096e8ba18e4f83946d366e5f83c2
Sam Fields <sam.fields94@gmail.com>
2014-10-04 11:29:58 -0700

    Added lasers, background, rotating platforms.

---
+++ Command~Balloon/Assets/rotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float rotateSpeed;
+
+	public bool clockwise;
+	public bool counterClockwise;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate (){
+		if (clockwise) {
+			transform.Rotate (Vector3.forward * -rotateSpeed);
+		} else if (counterClockwise) {
+			transform.Rotate (Vector3.forward * rotateSpeed);
+		}
+	}
+}
