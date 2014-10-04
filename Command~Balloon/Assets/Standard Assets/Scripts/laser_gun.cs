c8574fc8cc849a1b965858012ae4fc71519e6db4
Sam Fields <sam.fields94@gmail.com>
2014-10-04 14:29:51 -0700

    Added more levels, finish area

---
+++ Command~Balloon/Assets/Standard Assets/Scripts/laser_gun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laser_gun : MonoBehaviour {
+
+	public GameObject laser;
+	private GameObject shootFrom = null;
+	private GameObject fire = null;
+
+	private bool laserReady = true;
+	private float laserCD = 3f;
+
+	// Use this for initialization
+	void Start () {
+		shootFrom = GameObject.Find("barrelTip");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (laserReady) {
+			fire = Instantiate(laser, shootFrom.transform.position, Quaternion.FromToRotation(Vector3.up, transform.forward)) as GameObject;
+			laserReady = false;
+			Invoke ("laserOff", laserCD);
+		}
+	}
+
+	void laserOff () {
+		Destroy (fire);
+		Invoke ("laserOn", laserCD);
+	} 
+
+	void laserOn () {
+		laserReady = true;
+	}
+}
---
+++ Command~Balloon/Assets/finish.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finish : MonoBehaviour {
+
+	public float levelNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D hit) {
+		if (levelNumber == 1) {
+			Application.LoadLevel ("Level2");	
+		} else if (levelNumber == 2) {
+			Application.LoadLevel ("Level3");	
+		} else if (levelNumber == 3) {
+			Application.LoadLevel ("Level4");	
+		} else if (levelNumber == 4) {
+			Application.LoadLevel ("Level5");	
+		} else if (levelNumber == 5) {
+			Application.LoadLevel ("Level6");	
+		} 
+	}
+
+}
