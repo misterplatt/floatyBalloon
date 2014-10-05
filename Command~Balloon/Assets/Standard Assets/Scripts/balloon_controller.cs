4801d515570d22902e01851f25ab7732a03bd61f
Kenneth Thieu <kthieu@ucsc.edu>
2014-10-05 09:20:35 -0700

    BallonController.cs
    added my mobile code but commented them out. Up to you to separate for
    mobile version and balance it out.
    
    Also, I cannot figure out screen orientation. Add to how my USB to phone
    cable is crap to the point where I constantly disconnect at the
    slightest tilt makes testing painful.

--- Command~Balloon/Assets/Standard Assets/Scripts/balloon_controller.cs
+++ Command~Balloon/Assets/Standard Assets/Scripts/balloon_controller.cs
@@ -5,7 +5,6 @@ public class balloon_controller : MonoBehaviour {
 
 	public float floatSpeed;
 	public float dropPerClick;
-
 	public float addLRSpeed;
 	public float maxLRSpeed;
 
@@ -13,17 +12,52 @@ public class balloon_controller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+	//Mobile initializations
+		/*
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		Screen.autorotateToLandscapeLeft = true;
+		Screen.autorotateToLandscapeRight = false;
+		Screen.autorotateToPortrait = false;
+		Screen.autorotateToPortraitUpsideDown = false;
+		Screen.orientation = ScreenOrientation.AutoRotation;
+		*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
+	//Commented out what I tried for mobile controls
+		/*
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		Vector3 dir = Vector3.zero;
+
+		foreach(Touch tap in Input.touches) {
+			if (tap.phase == TouchPhase.Began) {
+				Debug.Log ("We touched");
+				rigidbody2D.AddForce(new Vector2(0, -100));
+			}
+		}
+
+		dir.x = Input.acceleration.x;
+		dir.y = -Input.acceleration.y;
+
+		if (dir.sqrMagnitude > 1) {
+			dir.Normalize ();
+		}
+
+		dir *= Time.deltaTime;
+		transform.Translate (dir * maxLRSpeed);
+		*/
+		
+		//Normal PC control
 		if (Input.GetKey (KeyCode.Space)) {
 			rigidbody2D.AddForce(new Vector2 (0, -30));		
 		}
+		
 	}
 
 	void FixedUpdate () {
+		
 		//L/R code
 		float moveH = Input.GetAxis ("Horizontal");
 		if(moveH > 0)
@@ -36,6 +70,7 @@ public class balloon_controller : MonoBehaviour {
 			if(rigidbody2D.velocity.x > -maxLRSpeed)
 				rigidbody2D.AddForce(new Vector2 (moveH * addLRSpeed, 0));
 		}
+		
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
