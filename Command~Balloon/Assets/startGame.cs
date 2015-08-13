ee4aab93fe7a3586b4b984816f82bcf9e0784cd8
Hayden Platt <misterplatt17@gmail.com>
2015-08-13 09:20:49 -0700

    Menu update

---
+++ Command~Balloon/Assets/startGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void loadGame(){
+		Application.LoadLevel ("Level1");
+	}
+
+	public void quitGame(){
+		Application.Quit ();
+	}
+}
